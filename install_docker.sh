e6a8e19f5b4ebf6e92eb1cb21e4bcb292ea3ea9e
Stan <shenzudan@51wanxue.com>
2020-09-25 17:40:18 +0800

    1. docker install shell.

---
+++ install_docker.sh
@@ -0,0 +1,20 @@
+yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+				  
+yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+  
+yum-config-manager \
+    --add-repo \
+    http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+
+yum install -y docker-ce docker-ce-cli containerd.io
+
+systemctl start docker
